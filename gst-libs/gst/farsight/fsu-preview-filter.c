89e2665bed7c6e9df87debb5058cbc69dd61281f
Youness Alaoui <youness.alaoui@collabora.co.uk>
2010-06-30 20:50:40 -0400

    Add include of config.h

--- gst-libs/gst/farsight/fsu-preview-filter.c
+++ gst-libs/gst/farsight/fsu-preview-filter.c
@@ -19,6 +19,12 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
+
+#ifdef HAVE_CONFIG_H
+#  include "config.h"
+#endif
+
+
 #include <gst/farsight/fsu-preview-filter.h>
 #include <gst/farsight/fsu-filter-helper.h>
 
--- gst-libs/gst/farsight/fsu-stream.c
+++ gst-libs/gst/farsight/fsu-stream.c
@@ -19,6 +19,12 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
+
+#ifdef HAVE_CONFIG_H
+#  include "config.h"
+#endif
+
+
 #include <gst/farsight/fsu-stream.h>
 #include <gst/farsight/fsu-stream-priv.h>
 #include <gst/farsight/fsu-session-priv.h>
